2eeb87722deeb99449d62589bc8d5abab9e3bb5f
MD M. Hasan <5706758689@GSA.GOV>
2023-08-09 15:41:28 -0400

    springboot-3.1 upgrade

--- src/main/java/gov/gsa/acr/authservice/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/gov/gsa/acr/authservice/config/JwtAuthenticationEntryPoint.java
@@ -3,9 +3,6 @@ package gov.gsa.acr.authservice.config;
 import java.io.IOException;
 import java.io.Serializable;
 
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -18,12 +15,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Se
 
 	private static final long serialVersionUID = -7858869558953243875L;
 
-//	@Override
-//	public void commence(HttpServletRequest request, HttpServletResponse response,
-//			AuthenticationException authException) throws IOException {
-//
-//		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
-//	}
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
--- src/main/java/gov/gsa/acr/authservice/config/WebSecurityConfig.java
+++ src/main/java/gov/gsa/acr/authservice/config/WebSecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,7 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
 
 	@Autowired
@@ -33,22 +33,6 @@ public class WebSecurityConfig {
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
 		return authenticationConfiguration.getAuthenticationManager();
 	}
-
-
-//	@Bean
-//	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-//
-//		httpSecurity.csrf().disable()
-//				.authorizeRequests().antMatchers("/**").permitAll().
-//				anyRequest().authenticated().and().
-//				// make sure we use stateless session; session won't be used to
-//				// store user's state.
-//						exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-//				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//		return httpSecurity.build();
-//		// Add a filter to validate the tokens with every request
-//		//	httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-//	}
 
 
 	@Bean
